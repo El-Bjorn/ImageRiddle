91e42603ada67f7c5b168a04082a4cc2906cafbc
Bjorn Chambless <bjorn@builtlight.org>
2016-04-05 14:35:13 -0700

    Initial commit. Displays image, getting touches

---
+++ ImageRiddle/TargetView.swift
@@ -0,0 +1,35 @@
+//
+//  TargetView.swift
+//  ImageRiddle
+//
+//  Created by BjornC on 4/5/16.
+//  Copyright © 2016 Builtlight. All rights reserved.
+//
+
+// The job of this uiview instance process touch events
+//    and host the cross-hairs layer
+
+import UIKit
+
+class TargetView: UIView {
+    var crosshairsLayer:CALayer?
+    var crosshairsPos:CGPoint?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // set transparent, so we still see the image
+        self.backgroundColor = UIColor.clearColor()
+    }
+    // Needed only to make xcode happy, we're using init(CGRect)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Overriding a UIResponder(UIView parent) method
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first
+        self.crosshairsPos = (touch?.locationInView(self))!
+        print("touch position: \(self.crosshairsPos!.x),\(self.crosshairsPos!.y)")
+        
+    }
+}
---
+++ ImageRiddle/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  ImageRiddle
+//
+//  Created by BjornC on 4/5/16.
+//  Copyright © 2016 Builtlight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var riddleImageView: UIImageView!
+    var targetView:TargetView? = nil
+    
+    @IBAction func positionSelectedButton(sender: UIButton) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // add the image to our UIImageView
+        //self.riddleImageView.contentMode = UIViewContentMode.ScaleAspectFit
+        self.riddleImageView.image = UIImage(named: "Seasame-St-Characters")
+        //
+        // create the TargetView and add it as a subview of riddleImageView
+        // TargetView's frame is the bounds of riddleImageView
+        let targetViewFrame = CGRectMake(0,0,self.riddleImageView.frame.size.width,self.riddleImageView.frame.size.height)
+        self.targetView = TargetView(frame: targetViewFrame)
+        
+        //riddleImageView.backgroundColor = UIColor.redColor()
+        riddleImageView.addSubview(self.targetView!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
