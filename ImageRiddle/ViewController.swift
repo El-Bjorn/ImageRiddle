34e079ff008e94e66b0a053306902052c1449a01
Bjorn Chambless <bjorn@builtlight.org>
2016-04-06 14:31:42 -0700

    Cleanup viewController

--- ImageRiddle/ViewController.swift
+++ ImageRiddle/ViewController.swift
@@ -1,36 +1,33 @@
 //
 //  ViewController.swift
 //  ImageRiddle
-//
-//  Created by BjornC on 4/5/16.
-//  Copyright © 2016 Builtlight. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var riddleImageView: UIImageView!
-    var targetView:TargetView? = nil
+    @IBOutlet var riddleImageView: UIImageView?
+    var targetView:TargetView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // make sure we have riddleImageView
+        if let imageView = self.riddleImageView {
+            imageView.image = UIImage(named: "Seasame-St-Characters")
+            
+            let targetViewFrame = CGRectMake(0, 0, imageView.frame.size.width, imageView.frame.size.height)
+            
+            self.targetView = TargetView(frame: targetViewFrame)
+            if (self.targetView != nil) {
+                imageView.addSubview(self.targetView!) // unwrapping ok since we checked it
+            }
+        }
+    }
     
     @IBAction func positionSelectedButton(sender: UIButton) {
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // add the image to our UIImageView
-        //self.riddleImageView.contentMode = UIViewContentMode.ScaleAspectFit
-        //self.riddleImageView.backgroundColor = UIColor.blueColor()
-        self.riddleImageView.image = UIImage(named: "Seasame-St-Characters")
-        //
-        // create the TargetView and add it as a subview of riddleImageView
-        // TargetView's frame is the bounds of riddleImageView
-        let targetViewFrame = CGRectMake(0,0,self.riddleImageView.frame.size.width,self.riddleImageView.frame.size.height)
-        self.targetView = TargetView(frame: targetViewFrame)
-        
-        //riddleImageView.backgroundColor = UIColor.redColor()
-        riddleImageView.addSubview(self.targetView!)
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
